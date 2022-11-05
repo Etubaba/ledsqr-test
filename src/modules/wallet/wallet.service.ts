b18a7ce2fbde6d523a1e883b338ec43719f9f788
Etumnu Prosper <johnikechukwu94@gmail.com>
2022-11-05 08:28:22 +0100

    transfer fund from wallet implementation

--- src/modules/wallet/wallet.service.ts
+++ src/modules/wallet/wallet.service.ts
@@ -1,6 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  NotAcceptableException,
+} from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectModel } from 'nest-knexjs';
+import { TransferFundDto } from './dto/transfer.dto';
 import { WalletDto } from './dto/wallet.dto';
 
 @Injectable()
@@ -60,6 +65,128 @@ export class WalletService {
       console.log(err.message);
     }
   }
+
+  //transfer fund from user balance (task 3)
+  async transferFund(transferDto: TransferFundDto) {
+    // try {
+    const {
+      sender_email,
+      amount,
+      sender_wallet_number,
+      receiver_wallet_number,
+      receiver_email,
+    } = transferDto;
+
+    //get sender wallet
+    const senderWallet =
+      sender_email !== undefined
+        ? await this.knex('wallets').where({ user_email: sender_email }).first()
+        : await this.knex('wallets')
+            .where({ wallet_number: sender_wallet_number })
+            .first();
+
+    //if sender wallet does not exist
+    if (!senderWallet) {
+      throw new NotFoundException('Sender account does not exist');
+    }
+
+    //check if wallet balance is sufficient for the transaction
+    if (senderWallet.amount < amount) {
+      throw new NotAcceptableException('Insufficient fund');
+    }
+
+    //get receiver wallet
+    const receiverWallet =
+      receiver_email !== undefined
+        ? await this.knex('wallets')
+            .where({ user_email: receiver_email })
+            .first()
+        : await this.knex('wallets')
+            .where({ wallet_number: receiver_wallet_number })
+            .first();
+
+    //if receiver wallet does not exist
+    if (!receiverWallet) {
+      throw new NotFoundException('Receiver account does not exist');
+    }
+
+    //deduct amount from sender wallet
+    if (sender_email !== undefined) {
+      await this.knex('wallets')
+        .where({ user_email: sender_email })
+        .update({
+          amount: senderWallet.amount - amount,
+        });
+    } else {
+      await this.knex('wallets')
+        .where({ wallet_number: sender_wallet_number })
+        .update({
+          amount: senderWallet.amount - amount,
+        });
+    }
+
+    //update sender wallet balance in users table
+    if (sender_email !== undefined) {
+      await this.knex('users')
+        .where('email', sender_email)
+        .update({
+          wallet_balance: senderWallet.amount - amount,
+        });
+    } else {
+      const userEmail = await this.knex('wallets')
+        .where({ wallet_number: sender_wallet_number })
+        .first();
+
+      await this.knex('users')
+        .where('email', userEmail.user_email)
+        .update({
+          wallet_balance: senderWallet.amount - amount,
+        });
+    }
+
+    //add amount to receiver wallet
+    if (receiver_email !== undefined) {
+      await this.knex('wallets')
+        .where({ user_email: receiver_email })
+        .update({
+          amount: receiverWallet.amount + amount,
+        });
+      //update receiver user table
+      await this.knex('users')
+        .where('email', receiver_email)
+        .update({
+          wallet_balance: receiverWallet.amount + amount,
+        });
+    } else {
+      await this.knex('wallets')
+        .where({ wallet_number: receiver_wallet_number })
+        .update({
+          amount: receiverWallet.amount + amount,
+        });
+      //update receiver user table
+      const userEmail = await this.knex('wallets')
+        .where({ wallet_number: receiver_wallet_number })
+        .first();
+
+      await this.knex('users')
+        .where('email', userEmail.user_email)
+        .update({
+          wallet_balance: receiverWallet.amount + amount,
+        });
+    }
+
+    const message =
+      receiver_email !== undefined
+        ? `Fund transfered to ${receiver_email} successfully`
+        : `Fund transfered to wallet with id:${receiver_wallet_number} successfully`;
+
+    return { status: true, msg: message };
+    // } catch (err: any) {
+    //   console.log(err.message);
+    // }
+  }
+
+  //withdraw from wallet balance
 
   async getWalletBalance(wallet_id: string | number) {
     const userWalletBalance = await this.knex('wallets')
